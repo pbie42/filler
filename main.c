6a92675793a568be2d67d75ac8b61568214595f5
callmepaulb <paul.w.bie@gmail.com>
2017-12-22 11:40:07 -0800

    [Add] - Struct setup

--- main.c
+++ main.c
@@ -10,10 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "filler.h"
+
+void				setup_struct(t_play *play)
+{
+	if (!play->plateau)
+		if (!(play->plateau = (t_plateau *)malloc(sizeof(t_plateau) * 1)))
+			ft_exit("Could not allocate plateau");
+	if (!play->piece)
+		if (!(play->piece = (t_piece *)malloc(sizeof(t_piece) * 1)))
+			ft_exit("Could not allocate piece");
+	play->player = 0;
+	play->plateau->x = 0;
+	play->plateau->y = 0;
+	play->piece->x = 0;
+	play->piece->y = 0;
+}
 
 int				main(void)
 {
-	ft_putendl("sup fool");
+	int			i;
+	char			*str;
+	char			**line;
+	t_play		*play;
+
+	if (!play)
+		if (!(play = (play *)malloc(sizeof(play) * 1)))
+			ft_exit("Could not allocate play");
+	setup_struct(play);
+	i = 0;
+	while (ft_get_next_line(0, &str) > 0)
+	{
+		line = ft_strsplit(str, ' ');
+
+	}
+	while(line[i])
+	{
+		ft_putendl("we are in here fuckkkk");
+		ft_putendl(line[i]);
+		i++;
+	}
+	
 	return (0);
 }
