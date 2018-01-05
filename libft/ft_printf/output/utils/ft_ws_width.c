36209565d5bcc34f0ca8037513e6ccc9b808df40
Paul BIE <pbie@e2r11p5.42.fr>
2018-01-05 15:55:12 +0100

    [Update] - Turned in printf version.

--- libft/ft_printf/output/utils/ft_ws_width.c
+++ libft/ft_printf/output/utils/ft_ws_width.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_percentage.c                                 :+:      :+:    :+:   */
+/*   ft_ws_width.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbie <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/07 15:16:39 by pbie              #+#    #+#             */
-/*   Updated: 2017/12/07 15:19:09 by pbie             ###   ########.fr       */
+/*   Created: 2018/01/04 15:16:39 by pbie              #+#    #+#             */
+/*   Updated: 2018/01/04 15:19:09 by pbie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-void				print_percentage(t_pf_item *pfi)
+int					ft_ws_width(wchar_t *ws)
 {
-	print_char(pfi, '%');
+	int				width;
+	int				i;
+
+	i = -1;
+	width = 0;
+	while (ws[++i])
+		width += get_wide_char_length(ws[i]);
+	return (width);
 }
